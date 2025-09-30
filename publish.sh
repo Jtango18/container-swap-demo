d6356ae6b6e6099dc7e6f116bf04478d90aaa961
JT <justin.taylor@octopus.com>
2025-09-30 12:02:02 +1000

    cleanup publish

--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker build --platform linux/x86_64 -t argodemoacr.azurecr.io/nginx:1.0 ./ColourContainers/.
-docker push argodemoacr.azurecr.io/my-colour-app:1.0
+source .env;
+docker build --platform linux/x86_64 -t argodemoacr.azurecr.io/my-colour-app:$VERSION ./ColourContainers/.
+docker push argodemoacr.azurecr.io/my-colour-app:$VERSION
