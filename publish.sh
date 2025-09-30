b4b0fcf56e1b1bd52cf65f0ff000176f63bc046b
JT <justin.taylor@octopus.com>
2025-09-30 11:56:33 +1000

    remove checkout login

--- publish.sh
+++ publish.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-git checkout main
 docker build --platform linux/x86_64 -t argodemoacr.azurecr.io/nginx:1.0 ./ColourContainers/.
-docker push argodemoacr.azurecr.io/nginx:1.0
-
-git checkout green
-docker build --platform linux/x86_64 -t argodemoacr.azurecr.io/nginx:2.0 ./ColourContainers/.
-docker push argodemoacr.azurecr.io/nginx:2.0
-
-git checkout blue
-docker build --platform linux/x86_64 -t argodemoacr.azurecr.io/nginx:3.0 ./ColourContainers/.
-docker push argodemoacr.azurecr.io/nginx:3.0
+docker push argodemoacr.azurecr.io/my-colour-app:1.0
